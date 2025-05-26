48beb80e45c85c2d24487a1e797bb8b3fd7b6d6a
perFuge11i <perfli@stud.ntnu.no>
2025-05-26 17:44:54 +0200

    Added very simple demo controller

---
+++ src/cube_pointer_controller/launch/controller.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from pathlib import Path
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory("cube_pointer_controller")
+    param_file = Path(pkg_share) / "config" / "params.yaml"
+
+    demo_move = Node(
+        package="cube_pointer_controller",
+        executable="simple_move",
+        name="cube_pointer_controller",
+        output="screen",
+        parameters=[param_file] if param_file.exists() else [],
+    )
+
+    return LaunchDescription([demo_move])
